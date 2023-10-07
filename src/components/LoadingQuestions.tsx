abfbff726898ca4465366d4516437ce1aa30aa83
Debayan Pratihar <121422042+debayanpratihar@users.noreply.github.com>
2023-10-07 20:15:28 +0530

    update

--- src/components/LoadingQuestions.tsx
+++ src/components/LoadingQuestions.tsx
@@ -6,7 +6,7 @@ type Props = { finished: boolean };
 
 const loadingTexts = [
   "Generating questions....",
-  "Unleashing the power of curiosity....",
+  "Unleashing the power of curiosity.....",
   "Diving deep into the ocean of questions....",
   "Harnessing the collective knowledge of the cosmos....",
   "Igniting the flame of wonder and exploration....",
